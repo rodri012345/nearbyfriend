97962bcada9f37dcf2984a38fa560310910db199
Rodry <rodrigo.quena14@gmail.com>
2024-04-01 23:10:01 -0400

    Documento Base con NavBar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav>
+      <Link to="/" className="title">
+        NearbyFriend
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/about">Inicio</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Quienes Somos</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Mi Cuenta</NavLink>
+        </li>
+        <li>
+          <NavLink to="/Mi Cuenta">Registro</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const Home = () => {
+  return <h1>Home</h1>;
+  <h1>Hello</h1>
+};
