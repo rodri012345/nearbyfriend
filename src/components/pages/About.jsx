40b35c6f532a9ed1693567a8b538468ed3cd10b0
SergioCanaviri <165413191+SergioCanaviri@users.noreply.github.com>
2024-04-05 17:13:00 -0400

    aprendiendo a hacer commits

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { useState } from "react";
 export const About = () => {
+  const [image,setImage ]=useState("");
+  const handleChange=(e)=>{
+    const urlImg = URL.createObjectURL(e.target.files[0]) 
+    console.log(urlImg)
+    console.log(e.target.files[0])
+    setImage(urlImg)
+  }
   return (
-    <input type="file" accept=".jpg,.jpeg,.png"></input>
+    <>
+    <input type="file" accept=".jpg,.jpeg,.png" onChange={(e)=>handleChange(e)}></input>
+    <img src={image} alt="cargando imagen"/>
+    
+    </>
   );
 };
