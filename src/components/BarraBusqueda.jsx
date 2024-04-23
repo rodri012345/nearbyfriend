bcfe8c885a381db8e7ce8fd36b21cca93233ba86
Luis Mejia Sanchez <165295212+Mejia87@users.noreply.github.com>
2024-04-23 05:17:36 -0400

    Actualice la pagina de inicio con los ultimos cambios realizados

---
+++ src/components/BarraBusqueda.jsx
@@ -0,0 +1,239 @@
+import React, { useState } from "react";
+import { Button, Select, Space } from "antd";
+import {
+  EnvironmentOutlined,
+  TeamOutlined,
+  CarOutlined,
+  CalendarOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
+
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+
+function BarraBusqueda() {
+  return (
+    <div style={{ padding: "50px"}}>
+      <div>
+      <h4 style={{fontFamily:'initial', marginLeft:'36px'}}>
+        Busca con quién y dónde quieres alquilar un amigo o encontrar una amiga
+      </h4>
+      </div>
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          
+        }}
+      >
+        <div>
+          <h6 style={{ fontFamily: "cursive" }}>
+            <i>
+              <EnvironmentOutlined />
+            </i>{" "}
+            ciudad
+          </h6>
+          <Select
+            defaultValue="Cochabamba"
+            style={{
+              width: 200,
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "Cochabamba",
+                label: "Cochabamba",
+              },
+              {
+                value: "La Paz",
+                label: "La Paz",
+              },
+              {
+                value: "Santa Cruz",
+                label: "Santa Cruz",
+              },
+              {
+                value: "Sucre",
+                label: "Sucre",
+              },
+              {
+                value: "Potosi",
+                label: "Potosi",
+              },
+              {
+                value: "Beni",
+                label: "Beni",
+              },
+              {
+                value: "Pando",
+                label: "Pando",
+              },
+              {
+                value: "Tarija",
+                label: "Tarija",
+              },
+              {
+                value: "Oruro",
+                label: "Oruro",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <h6 style={{ fontFamily: "cursive" }}>
+            <TeamOutlined /> genero
+          </h6>
+          <Select
+            defaultValue="Ambos"
+            style={{
+              width: 200,
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "Chica",
+                label: "Chica",
+              },
+              {
+                value: "Chico",
+                label: "Chico",
+              },
+              {
+                value: "Ambos",
+                label: "Ambos",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <h6 style={{ fontFamily: "cursive" }}>
+            <CarOutlined /> Hobbies/gustos
+          </h6>
+          <Select
+            defaultValue="Cualquiera"
+            style={{
+              width: 200,
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "Cualquiera",
+                label: "Cualquiera",
+              },
+              {
+                value: "Cantar",
+                label: "Cantar",
+              },
+              {
+                value: "Bailar",
+                label: "Bailar",
+              },
+              {
+                value: "Comer",
+                label: "Comer",
+              },
+              {
+                value: "Peliculas",
+                label: "Peliculas",
+              },
+              {
+                value: "Cine",
+                label: "Cine",
+              },
+              {
+                value: "Leer",
+                label: "Leer",
+              },
+              {
+                value: "Pasear",
+                label: "Pasear",
+              },
+              {
+                value: "Arte",
+                label: "Arte",
+              },
+              {
+                value: "Futbol",
+                label: "Futbol",
+              },
+              {
+                value: "Musica",
+                label: "Musica",
+              },
+              {
+                value: "Mascotas",
+                label: "Mascotas",
+              },
+              {
+                value: "Anime",
+                label: "Anime",
+              },
+              {
+                value: "Estudiar",
+                label: "Estudiar",
+              },
+              {
+                value: "Autos",
+                label: "Autos",
+              },
+              {
+                value: "Deportes",
+                label: "Deportes",
+              },
+              {
+                value: "gimnasio",
+                label: "gimnasio",
+              },
+            ]}
+          />
+        </div>
+        <div>
+          <h6 style={{ fontFamily: "cursive" }}>
+            <CalendarOutlined /> edad
+          </h6>
+          <Select
+            defaultValue="Cualquiera"
+            style={{
+              width: 200,
+            }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "Cualquiera",
+                label: "Cualquiera",
+              },
+              {
+                value: "entre 18 y 25",
+                label: "entre 18 y 25",
+              },
+              {
+                value: "entre 25 y 35",
+                label: "entre 25 y 35",
+              },
+              {
+                value: "entre 35 y 45",
+                label: "entre 35 y 45",
+              },
+              {
+                value: "entre 45 y 65",
+                label: "entre 45 y 65",
+              },
+              {
+                value: "mas de 65",
+                label: "mas de 65",
+              },
+            ]}
+          />
+        </div>
+        <Space>
+          <Button type="primary" icon={<SearchOutlined />}>
+            Buscar
+          </Button>
+        </Space>
+      </div>
+    </div>
+  );
+}
+export default BarraBusqueda;
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import Footer from '../Fotter'
+import Footer from "../Fotter";
+import Carrusel from "../Carrusel";
 
+import BarraBusqueda from "../BarraBusqueda";
+import { Layout } from "antd";
+import Enlaces from "../Enlaces";
+
+
+const { Content } = Layout;
 
 export const About = () => {
   return (
     <>
-      
-      <Footer />
+      <Content style={{padding:'20px', backgroundColor:'#FBF3D5'}}>
+        <div
+          style={{
+            backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/nearbyfriend.appspot.com/o/inicio.jpg?alt=media&token=1fd8d284-415f-4546-808d-a85098b00fc7")` /* 'https://www.elplural.com/uploads/s1/11/08/54/6/simon-maage-tximrx3gc-g-unsplash.jpeg' */,
+            backgroundSize: "100% 100%",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "700px",
+            
+          }}
+        ></div>
+        <div style={{backgroundColor:'#DDDDDD'}}>
+        <BarraBusqueda/>
+        </div>
+        <Enlaces/>
+        <h1>Ultimos Amigos Registrados</h1>
+        <Carrusel />
+      </Content>
+      <Footer/>
     </>
   );
 };
