3ea3b2f67ca512ea318a4ebc7bc603e2de26fe1d
Luis Mejia Sanchez <165295212+Mejia87@users.noreply.github.com>
2024-04-29 15:25:27 -0400

    se actualizo  los cambios de main a Luis

--- src/components/pages/RegistroAmigo.jsx
+++ src/components/pages/RegistroAmigo.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import Footer from '../Fotter'
-<<<<<<< HEAD
-import RegistroA from "../RegistroA";
-import SubirFotoAmigo from "../subirFotoAmigo";
-export const RegistroAmigo = () => {
-    return (
-        <>
-        <RegistroA />
-        <SubirFotoAmigo/>
-=======
 import Registro from "../Registro";
 export const RegistroAmigo = () => {
     return (
         <>
         <Registro />
->>>>>>> main
         <Footer />
         
         </>
