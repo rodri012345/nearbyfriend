7ae92990227e30a8ccc30c03eeac5b1f7f013e02
SergioCanaviri <165413191+SergioCanaviri@users.noreply.github.com>
2024-04-29 23:31:46 -0400

    Hice cambios en mi rama

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,23 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { useState } from "react";
-export const About = () => {
-  const [image,setImage ]=useState("");
-  const handleChange=(e)=>{
-    const urlImg = URL.createObjectURL(e.target.files[0]) 
-    console.log(urlImg)
-    console.log(e.target.files[0])
-    setImage(urlImg)
-  }
-  return (
-    <>
-    <input type="file" accept=".jpg,.jpeg,.png" onChange={(e)=>handleChange(e)}></input>
-    <img src={image} alt="cargando imagen"/>
-    
-    </>
-  );
-};
-=======
 import Footer from '../Fotter'
 
 
@@ -29,4 +10,3 @@ export const About = () => {
     </>
   );
 };
->>>>>>> main
