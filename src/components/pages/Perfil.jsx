906572f6fecc061c298688b4d01f68367ff46642
Eddy Fernandez <arbarest56@gmail.com>
2024-04-29 23:04:45 -0400

    Perfiles

--- src/components/pages/Perfil.jsx
+++ src/components/pages/Perfil.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Footer from "../Fotter";
-import App2 from "../Alquilar";
 import Alquilar from "../Alquilar";
+import PerfilAmigo from "../PerfilAmigo";
+import PerfilCliente from "../PerfilCliente";
+import App from "../Solicitud";
 export const Perfil = () => {
     return (
         <>
+     
         
         
-        <Alquilar />
+        <PerfilCliente clienteId="AW9TuMqYHHJZRKlEF0qa"/>
         <Footer />
+        
 
         </>
     )
