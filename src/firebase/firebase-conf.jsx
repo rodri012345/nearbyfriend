ed03a4d014b9de80785e894cd974ab69603d4d64
Luis Mejia Sanchez <165295212+Mejia87@users.noreply.github.com>
2024-05-02 20:41:52 -0400

    se cambio al base de datos de respaldo

--- src/firebase/firebase-conf.jsx
+++ src/firebase/firebase-conf.jsx
@@ -2,14 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+// const firebaseConfig = {
+// apiKey: "AIzaSyD3YIyAIGmwyRz4fLmZVpQbnYJInQiOD4Y",
+// authDomain: "nearbyfriend.firebaseapp.com",
+// projectId: "nearbyfriend",
+// storageBucket: "nearbyfriend.appspot.com",
+// messagingSenderId: "543664631711",
+// appId: "1:543664631711:web:cf2588ed5ddfb4c4e657aa",
+// measurementId: "G-PNRXJRYRTG"
+// };
 const firebaseConfig = {
-apiKey: "AIzaSyD3YIyAIGmwyRz4fLmZVpQbnYJInQiOD4Y",
-authDomain: "nearbyfriend.firebaseapp.com",
-projectId: "nearbyfriend",
-storageBucket: "nearbyfriend.appspot.com",
-messagingSenderId: "543664631711",
-appId: "1:543664631711:web:cf2588ed5ddfb4c4e657aa",
-measurementId: "G-PNRXJRYRTG"
+  apiKey: "AIzaSyCtHYCYdUN17vIwhPPvLADefPFEbKk9Cg8",
+  authDomain: "hospedaje-react-dd174.firebaseapp.com",
+  projectId: "hospedaje-react-dd174",
+  storageBucket: "hospedaje-react-dd174.appspot.com",
+  messagingSenderId: "1066849059054",
+  appId: "1:1066849059054:web:7da379cd92ff175e52f3ff"
 };
 
 
